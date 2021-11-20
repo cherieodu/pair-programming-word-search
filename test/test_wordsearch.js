2ea16e0f7f49668347c44e08af34b28a3aead3e6
Chérie Oduwole <bellapette0@gmail.com>
2021-11-20 22:35:20 +0000

    implemented a word search solver that receives a 2D array of letters and a word, and checks to find the word horizontally and vertically.

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present (horizontal check)", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -32,6 +32,22 @@ describe("#wordSearch()", function() {
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
     ], 'SEINFELD')
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present (vertical backwards check)", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'E', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'I', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'R', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'É', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'H', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'C', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CHÉRIE')
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,89 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  let checkHorizontal = searchHorizontal(letters, word);
+  
+  if (checkHorizontal === true) {
+    return true;
+  } else {
+    let checkBackHorizontal = searchBackHorizontal(letters, word);
+  
+    if (checkBackHorizontal) {
+      return true;
+    } else {
+      let checkVertical = searchVertical(letters, word);
+  
+      if (checkVertical) {
+        return true;
+      } else {
+        let checkBackVertical = searchBackVertical(letters, word);
+  
+        return (checkBackVertical ? true : false);
+      }
     }
-}
+  }
+    
+};
+  
+//Function to check horizontally.
+const searchHorizontal = (letters, word) => {
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  let horizontalCheck = searchWord(horizontalJoin, word);
+  return horizontalCheck;
+  
+};
+  
+//Function to check horizontally but backwards.
+const searchBackHorizontal = (letters, word) => {
+  
+  const horizontalBackJoin = letters.map(ls => {
+    let newWord = ls.join('');
+    let reversedWord = newWord.split('').reduce((a, b)=> b + a, '');
+    return reversedWord;
+  });
+  
+  let horizontalBackCheck = searchWord(horizontalBackJoin, word);
+  return horizontalBackCheck;
+  
+};
+  
+//Function to check vertically.
+const searchVertical = (letters, word) => {
+  
+  const verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let joinedWord = '';
+    for (let array of letters) {
+      joinedWord += array[i];
+    } verticalJoin.push(joinedWord);
+  } let verticalCheck = searchWord(verticalJoin, word);
+  
+  return verticalCheck;
+  
+};
+  
+//Function to check vertically but backwards.
+const searchBackVertical = (letters, word) => {
+  
+  const verticalBackJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let joinedWord = '';
+    for (let a = letters.length - 1; a >= 0; a--) {
+      joinedWord += letters[a][i];
+    } verticalBackJoin.push(joinedWord);
+  }
+    
+  let verticalBackCheck = searchWord(verticalBackJoin, word);
+  
+  return verticalBackCheck;
+};
+  
+const searchWord = (joined, word) => {
+  for (let l of joined) {
+    if (l.includes(word)) {
+      return true;
+    }
+  } return false;
+};
+  
 
-module.exports = wordSearch
+module.exports = wordSearch;
